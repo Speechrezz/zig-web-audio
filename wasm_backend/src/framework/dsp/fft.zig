28d85f1de5c4a00e54861b73a7903ef1e061c6d9
Speechrezz <thespeechlessmusic@gmail.com>
2026-01-22 16:48:25 -0800

    Updated FFT

--- wasm_backend/src/framework/dsp/fft.zig
+++ wasm_backend/src/framework/dsp/fft.zig
@@ -10,21 +10,11 @@ fn reverseBits(x_in: anytype, num_bits: usize) @TypeOf(x_in) {
     return r;
 }
 
-// Uses the Radix-2 Cooley-Tukey algorithm. Not the fastest implementation.
-pub fn fftInplace(comptime FloatType: type, array: []std.math.Complex(FloatType), inverse: bool) void {
+fn fftInPlaceNoBitReversal(comptime FloatType: type, array: []std.math.Complex(FloatType), inverse: bool) void {
     const Complex = std.math.Complex(FloatType);
 
     const N = array.len;
     std.debug.assert(std.math.isPowerOfTwo(N));
-
-    // 1. Bit-reversal
-    const num_bits = std.math.log2_int(usize, N);
-    for (0..N) |i| {
-        const j = reverseBits(i, num_bits);
-        if (j > i) {
-            std.mem.swap(Complex, &array[i], &array[j]);
-        }
-    }
 
     // 2. Iterative butterfly
     var m: usize = 2;
@@ -50,15 +40,52 @@ pub fn fftInplace(comptime FloatType: type, array: []std.math.Complex(FloatType)
     // 3. Normalize if inverse
     if (inverse) {
         const invN = 1.0 / @as(FloatType, @floatFromInt(N));
-        for (0..N) |i| {
-            const v = &array[i];
+        for (array) |*v| {
             v.re *= invN;
             v.im *= invN;
         }
     }
 }
 
-test "FFT test" {
+// Uses the Radix-2 Cooley-Tukey algorithm. Not the fastest implementation.
+pub fn fftInPlace(comptime FloatType: type, array: []std.math.Complex(FloatType), inverse: bool) void {
+    const Complex = std.math.Complex(FloatType);
+    const N = array.len;
+
+    // 1. Bit-reversal
+    const num_bits = std.math.log2_int(usize, N);
+    for (0..N) |i| {
+        const j = reverseBits(i, num_bits);
+        if (j > i) {
+            std.mem.swap(Complex, &array[i], &array[j]);
+        }
+    }
+
+    fftInPlaceNoBitReversal(FloatType, array, inverse);
+}
+
+// Uses the Radix-2 Cooley-Tukey algorithm. Not the fastest implementation.
+// Maybe should use the Stockham algorithm instead?
+pub fn fftOutOfPlace(
+    comptime FloatType: type,
+    in: []const std.math.Complex(FloatType),
+    out: []std.math.Complex(FloatType),
+    inverse: bool,
+) void {
+    const N = in.len;
+    std.debug.assert(in.len == out.len);
+
+    // 1. Bit-reversal
+    const num_bits = std.math.log2_int(usize, N);
+    for (0..N) |i| {
+        const j = reverseBits(i, num_bits);
+        out[j] = in[i];
+    }
+
+    fftInPlaceNoBitReversal(FloatType, out, inverse);
+}
+
+test "FFT in-place test" {
     const Complex = std.math.Complex(f32);
 
     // FFT of an impulse
@@ -69,7 +96,7 @@ test "FFT test" {
         v.* = .{ .re = 0.0, .im = 0.0 };
     }
 
-    fftInplace(f32, &values1, false);
+    fftInPlace(f32, &values1, false);
 
     for (values1) |v| {
         try std.testing.expectApproxEqRel(1.0, v.re, 1e-5);
@@ -85,7 +112,7 @@ test "FFT test" {
         .{ .re = 0.0, .im = 0.0 },
     };
 
-    fftInplace(f32, &values2, false);
+    fftInPlace(f32, &values2, false);
 
     for (values2, 0..) |v, i| {
         if (@mod(i, 2) == 0) {
@@ -110,10 +137,73 @@ test "FFT test" {
 
     const values3_copy = values3;
 
-    fftInplace(f32, &values3, false);
-    fftInplace(f32, &values3, true);
+    fftInPlace(f32, &values3, false);
+    fftInPlace(f32, &values3, true);
 
     for (values3, values3_copy) |v1, v2| {
+        try std.testing.expectApproxEqRel(v1.re, v2.re, 1e-5);
+        try std.testing.expectApproxEqRel(v1.im, v2.im, 1e-5);
+    }
+}
+
+test "FFT out-of-place test" {
+    const Complex = std.math.Complex(f32);
+
+    // FFT of an impulse
+
+    var values1: [32]Complex = undefined;
+    var values1_out: [32]Complex = undefined;
+    values1[0] = .{ .re = 1.0, .im = 0.0 };
+    for (values1[1..]) |*v| {
+        v.* = .{ .re = 0.0, .im = 0.0 };
+    }
+
+    fftOutOfPlace(f32, &values1, &values1_out, false);
+
+    for (values1_out) |v| {
+        try std.testing.expectApproxEqRel(1.0, v.re, 1e-5);
+        try std.testing.expectApproxEqAbs(0.0, v.im, 1e-5);
+    }
+
+    // FFT of cosine
+
+    var values2 = [_]Complex{
+        .{ .re = 1.0, .im = 0.0 },
+        .{ .re = 0.0, .im = 0.0 },
+        .{ .re = -1.0, .im = 0.0 },
+        .{ .re = 0.0, .im = 0.0 },
+    };
+    var values2_out: [4]Complex = undefined;
+
+    fftOutOfPlace(f32, &values2, &values2_out, false);
+
+    for (values2_out, 0..) |v, i| {
+        if (@mod(i, 2) == 0) {
+            try std.testing.expectApproxEqAbs(0.0, v.re, 1e-5);
+        } else {
+            try std.testing.expectApproxEqRel(2.0, v.re, 1e-5);
+        }
+
+        try std.testing.expectApproxEqAbs(0.0, v.im, 1e-5);
+    }
+
+    // FFT and inverse FFT
+
+    var values3: [32]Complex = undefined;
+    var values3_fft: [32]Complex = undefined;
+    var values3_ifft: [32]Complex = undefined;
+    var i: f32 = 1.0;
+    for (&values3) |*v| {
+        v.re = i;
+        i += 1.0;
+        v.im = i;
+        i += 1.0;
+    }
+
+    fftOutOfPlace(f32, &values3, &values3_fft, false);
+    fftOutOfPlace(f32, &values3_fft, &values3_ifft, true);
+
+    for (values3, values3_ifft) |v1, v2| {
         try std.testing.expectApproxEqRel(v1.re, v2.re, 1e-5);
         try std.testing.expectApproxEqRel(v1.im, v2.im, 1e-5);
     }
