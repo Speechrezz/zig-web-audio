9199fa29d8cfc422497b94c85a33c07b9996a79e
Speechrezz <thespeechlessmusic@gmail.com>
2026-04-08 16:27:52 -0700

    Working serialization of ChordicProcessor

--- wasm_backend/src/chordic/processors/ChordicProcessor.zig
+++ wasm_backend/src/chordic/processors/ChordicProcessor.zig
@@ -133,6 +133,19 @@ pub fn getTrack(self: *@This(), index: usize) *TrackProcessor {
     return self.track_list.items[index];
 }
 
+pub fn toJsonSpec(self: *@This(), write_stream: *std.json.Stringify) !void {
+    try write_stream.beginObject();
+
+    try write_stream.objectField("tracks");
+    try write_stream.beginArray();
+    for (self.track_list.items) |track| {
+        try track.toJsonSpec(write_stream);
+    }
+    try write_stream.endArray();
+
+    try write_stream.endObject();
+}
+
 pub fn save(self: *@This(), ctx: *const anyopaque, write_stream: *std.json.Stringify) !void {
     try write_stream.beginObject();
 
@@ -163,4 +176,94 @@ pub fn load(self: *@This(), allocator: std.mem.Allocator, ctx: *anyopaque, parse
         try track.load(allocator, ctx, track_json);
         try self.track_list.append(allocator, track);
     }
+}
+
+test "toJsonSpec" {
+    const TestingAudioProcessor = framework.testing.TestingAudioProcessor;
+
+    const allocator = std.testing.allocator;
+
+    var track = try TrackProcessor.create(allocator);
+    const generator = try TestingAudioProcessor.create(allocator);
+    track.generator_device = TrackProcessor.Device.init(generator);
+
+    var processor: @This() = undefined;
+    processor.init();
+    defer processor.deinit(allocator);
+    try std.testing.expect(processor.insertTrack(allocator, 0, track));
+
+    // Stringify
+    var out: std.io.Writer.Allocating = .init(allocator);
+    defer out.deinit();
+    var write_stream: std.json.Stringify = .{
+        .writer = &out.writer,
+        .options = .{ .whitespace = .indent_2 },
+    };
+
+    try processor.toJsonSpec(&write_stream);
+    // std.debug.print("{s}\n", .{out.written()});
+
+    try std.testing.expect(std.mem.indexOf(u8, out.written(), "\"tracks\":") != null);
+    try std.testing.expect(std.mem.indexOf(u8, out.written(), "\"kind\": \"trackProcessor\"") != null);
+    try std.testing.expect(std.mem.indexOf(u8, out.written(), "\"kind\": \"testProcessor\"") != null);
+    try std.testing.expect(std.mem.indexOf(u8, out.written(), "\"parameters\":") != null);
+    try std.testing.expect(std.mem.indexOf(u8, out.written(), "\"generator\": {") != null);
+    try std.testing.expect(std.mem.indexOf(u8, out.written(), "\"effects\": []") != null);
+}
+
+test "save/load" {
+    const TestingAudioProcessor = framework.testing.TestingAudioProcessor;
+    const TestingProcessorRegistry = framework.testing.TestingProcessorRegistry;
+
+    const allocator = std.testing.allocator;
+    var context = SerializationContext.init(TestingProcessorRegistry.createInstance());
+    defer context.deinit(allocator);
+
+    // --Saving--
+
+    var track1 = try TrackProcessor.create(allocator);
+    const generator_dummy = try TestingAudioProcessor.create(allocator);
+    track1.generator_device = TrackProcessor.Device.init(generator_dummy);
+
+    var processor1: @This() = undefined;
+    processor1.init();
+    defer processor1.deinit(allocator);
+    try std.testing.expect(processor1.insertTrack(allocator, 0, track1));
+
+    // Stringify
+    var out1: std.io.Writer.Allocating = .init(allocator);
+    defer out1.deinit();
+    var write_stream1: std.json.Stringify = .{
+        .writer = &out1.writer,
+        .options = .{ .whitespace = .indent_2 },
+    };
+
+    try processor1.save(&context, &write_stream1);
+    // std.debug.print("out1:\n{s}\n", .{out1.written()});
+
+    try std.testing.expect(std.mem.indexOf(u8, out1.written(), "\"kind\": \"testProcessor\"") != null);
+
+    // --Loading--
+
+    var processor2: @This() = undefined;
+    processor2.init();
+    defer processor2.deinit(allocator);
+
+    const parsed = try std.json.parseFromSlice(std.json.Value, allocator, out1.written(), .{});
+    defer parsed.deinit();
+
+    try processor2.load(allocator, &context, &parsed.value);
+
+    // Stringify
+    var out2: std.io.Writer.Allocating = .init(allocator);
+    defer out2.deinit();
+    var write_stream2: std.json.Stringify = .{
+        .writer = &out2.writer,
+        .options = .{ .whitespace = .indent_2 },
+    };
+
+    try processor2.save(&context, &write_stream2);
+    // std.debug.print("\nout2:\n{s}\n", .{out2.written()});
+
+    try std.testing.expectEqualSlices(u8, out1.written(), out2.written());
 }
